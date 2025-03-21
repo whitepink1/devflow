5f072fbebf37d14ad3201f4adee35aac7a4aaf87
whitepink1 <kiruhat9@gmail.com>
2025-03-21 19:56:45 +0200

    adding username commit

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -58,7 +58,7 @@ export async function POST(req: Request) {
     const mongoUser = await createUser({
         clerkId: id,
         name: `${first_name}${last_name ? `${last_name}` : ''}`,
-        username: username!,
+        username: username || email_addresses[0].email_address.split("@")[0],
         email: email_addresses[0].email_address,
         picture: image_url,
     });
