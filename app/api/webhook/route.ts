17ad989f788db4cf4bf72e525f04c037c8378d9f
whitepink1 <kiruhat9@gmail.com>
2025-03-21 19:42:34 +0200

    new one  commit

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,5 +1,5 @@
 import { Webhook } from 'svix'
-import { headers } from 'next/headers'
+//import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.action'
